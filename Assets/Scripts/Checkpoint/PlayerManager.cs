dd9ef3aeeb9c7896a67c4d3beffed0f567c964de
anjaerza <andresjaeraso@gmail.com>
2019-09-27 21:50:12 -0500

    BETA

---
+++ Assets/Scripts/Checkpoint/PlayerManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    private GameMaster gm;
+    private HPmanager hpManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gm = GameObject.FindGameObjectsWithTag
+        hpManager = transform.GetComponent<HPmanager>();
+        gm = FindObjectOfType<GameMaster>();
+        transform.position = gm.ultimocheckpos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hpManager.currentlifePoints <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+
+    }
+
+   
+}
